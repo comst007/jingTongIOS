3663085a309514f3af2318930ca5b6bc59f2734d
LiuZHao <comst@comstdeMacBook-Pro.local>
2015-12-07 02:37:57 +0800

    save by sqlite3

--- LZDataSaveFun/LZDataSaveFun/ViewController.m
+++ LZDataSaveFun/LZDataSaveFun/ViewController.m
@@ -8,12 +8,14 @@
 
 #import "ViewController.h"
 #import "LZFileContent.h"
-#define kFileName @"data.archive"
+#import <sqlite3.h>
+#define kFileName @"data.sqlite"
 
 @interface ViewController ()
 @property (strong, nonatomic) IBOutletCollection(UITextField) NSArray *textFields;
 
 @property (nonatomic, strong) NSMutableArray *contents;
+@property (nonatomic, assign) sqlite3  *db;
 @end
 
 @implementation ViewController
@@ -24,7 +26,7 @@
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationWillResignActive:) name:UIApplicationWillResignActiveNotification object:nil];
     if ([self fileExist]) {
-        [self loadArchiveData];
+        [self loadSqliteData];
     }
     
 }
@@ -38,6 +40,34 @@
         tf.text = lines[i];
     }
     
+}
+
+- (void)loadSqliteData{
+    
+    sqlite3 *db ;
+    
+    if (sqlite3_open([[self filePath] UTF8String], &db) != SQLITE_OK) {
+        NSLog(@"open db failed");
+        return ;
+    }
+    
+    NSString *querySQL = @"SELECT ROW, FIELD_DATA FROM FIELDS ORDER BY ROW";
+    sqlite3_stmt *stmt;
+    if (sqlite3_prepare_v2(db, [querySQL UTF8String], -1, &stmt, NULL) == SQLITE_OK) {
+        
+        while (sqlite3_step(stmt) == SQLITE_ROW) {
+            int row = sqlite3_column_int(stmt, 0);
+            char* ctext = (char*)sqlite3_column_text(stmt, 1);
+            
+            UITextField *tf = self.textFields[row];
+            
+            tf.text = [NSString stringWithUTF8String:ctext];
+            
+        }
+        sqlite3_finalize(stmt);
+    }
+    sqlite3_close(db);
+
 }
 
 - (void)loadArchiveData{
@@ -71,7 +101,7 @@
 
 - (void)applicationWillResignActive:(NSNotification *)noti{
     
-    [self saveDataByArchive];
+    [self saveBySqlite3];
 }
 
 
@@ -124,4 +154,55 @@
     
 }
 
+- (BOOL)sqlitePrepare{
+    
+    if (![self fileExist]) {
+        sqlite3 *db ;
+        
+        if (sqlite3_open([[self filePath] UTF8String], &db) != SQLITE_OK) {
+            NSLog(@"create db failed");
+            return NO;
+        }
+        
+        NSString *createTableSQL = @"CREATE TABLE IF NOT EXISTS FIELDS(ROW INTEGER PRIMERY KEY, FIELD_DATA TEXT);";
+        
+        char *errMSG = NULL;
+        
+        if (sqlite3_exec(db, [createTableSQL UTF8String], NULL, NULL, &errMSG) != SQLITE_OK) {
+            sqlite3_close(db);
+            NSLog(@"create table error %s", errMSG);
+            return NO;
+        }
+        self.db = db;
+    }
+    return YES;
+}
+
+- (void)saveBySqlite3{
+    
+    if (![self sqlitePrepare]) {
+        return;
+    }
+    for (int i = 0; i < 4; i ++) {
+        UITextField *tf = self.textFields[i];
+        char *updateSQL = "INSERT OR REPLACE INTO FIELDS (ROW, FIELD_DATA) VALUES(?, ?);";
+        char *errMSG = NULL;
+        sqlite3_stmt *stmt;
+        if (sqlite3_prepare_v2(self.db, updateSQL, -1, &stmt, NULL) == SQLITE_OK) {
+            sqlite3_bind_int(stmt, 1, i);
+            sqlite3_bind_text(stmt, 2, [tf.text UTF8String], -1, NULL);
+        }
+        if (sqlite3_step(stmt) != SQLITE_DONE) {
+            NSLog(@"update table fail");
+            sqlite3_finalize(stmt);
+            sqlite3_close(self.db);
+            return ;
+            
+        }
+        sqlite3_finalize(stmt);
+    }
+    sqlite3_close(self.db);
+    
+    
+}
 @end
